0e00f9cde16f19960a1d60fd66e6062250a5f3e8
LigmayoSIT <20253520@s.ubaguio.edu>
2026-09-24 22:40:34 +0800

    feat: added a new component(WIP).

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { TrainerDisplay } from './trainer-display/trainer-display';
 import { KantoRegion } from './pokemon-gym-leaders/kanto-region/kanto-region';
 import { JohtoRegion } from './pokemon-gym-leaders/johto-region/johto-region';
+import { PokemonInputOutput } from './pokemon-input-output/pokemon-input-output';
 
 @Component({
-  imports: [RouterOutlet, TrainerDisplay, KantoRegion, JohtoRegion],
+  imports: [RouterOutlet, TrainerDisplay, KantoRegion, JohtoRegion, PokemonInputOutput],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
+  standalone: true
 })
 export class App {
   title = 'Hello GitHub! This is my first Angular push.';
+
+  //Trainer Data (Test)
+  trainerNames = [
+    {name: 'Ash Ketchum', section: 'Pallet Town'},
+    {name: 'Misty', section: 'Cerulean City'},
+    {name: 'Brock', section: 'Pewter City'}
+  ];
+  //State Message
+  message = signal('Please Choose a Trainer List');
+  //Event Handler
+  markConfirmed(trainerName: string) {
+    this.message.set(`Trainer ${trainerName} list chosen.`);
+  }
 }
--- src/app/pokemon-input-output/pokemon-input-output.ts
+++ src/app/pokemon-input-output/pokemon-input-output.ts
@@ -7,4 +7,14 @@ import { Component, input, output } from '@angular/core';
   styleUrl: './pokemon-input-output.css',
   templateUrl: './pokemon-input-output.html',
 })
-export class PokemonInputOutput {}
+export class PokemonInputOutput {
+  //Inputs
+  name = input.required<string>();
+  status = input<string>('Pokemon Trainer');
+  //Outputs
+  statusConfirmed = output<string>();
+  //Method
+  markConfirmed(){
+    this.statusConfirmed.emit(this.name());
+  }
+}
